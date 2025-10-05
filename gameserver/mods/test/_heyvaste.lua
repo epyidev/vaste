26c9b3a408bbf7a96d665e3b3bf1e76c06ab8013
Epyi <68242948+epyidev@users.noreply.github.com>
2025-10-05 10:49:12 +0200

    Remove test player state files and update mod
    Deleted test-player.json and its backup from client_state, and updated _heyvaste.lua in the test mod. Also removed package.json and updated client_state/2.json and its backup, likely to clean up unused files and update test data.

--- gameserver/mods/test/_heyvaste.lua
+++ gameserver/mods/test/_heyvaste.lua
@@ -3,10 +3,8 @@
 
 name("test")
 description("This is a test mod")
-version("beta 1.0.0")
+version("1.0.0")
 author("vaste")
 
 load_client_script("client/main.lua")
--- try Lua first (requires fengari); fallback to JS if Lua fails or is unavailable
-load_server_script("server/debug_print.lua")
 load_server_script("server/main.lua")
